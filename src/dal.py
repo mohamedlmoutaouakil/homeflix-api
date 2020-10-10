0e80e24b214a0bbb93117c49a607e136d63597b6
Mohamed Lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-10-10 22:38:50 +0100

    Added search by dirname and add media functions to dal

--- src/dal.py
+++ src/dal.py
@@ -1,6 +1,7 @@
 import json
 import os
-from src.exceptions import NotFoundException, WrongMediaTypeException
+from os.path import dirname
+from src.exceptions import InvalidMediaObjectException, NotFoundException, WrongMediaTypeException
 
 current_dir = os.getcwd()
 db_path = os.path.join(current_dir, 'databases', 'medias_db.json')
@@ -9,12 +10,16 @@ with open(db_path) as f:
   medias_db = json.load(f)
 
 media_types = ['all', 'movies', 'series', 'animes']
+JSON_FIELDS = {'actors', 'awards', 'country', 'director', 'genre', 
+  'imdbRating', 'language', 'mediaType', 
+  'plot', 'poster', 'rated', 'ratings', 'released', 
+  'runtime', 'title', 'writer', 'year', 'url', 'dirname'} # Set of the json fields in media object
 
 def get_medias_by_type(media_type):
   if media_type not in media_types:
     raise WrongMediaTypeException
   if media_type == 'all':
-    return medias_db['medias']
+    return list(medias_db['medias'].values())
   return list(filter(lambda m: m['mediaType'] == media_type, medias_db['medias'].values()))
 
 def get_media_by_id(media_id):
@@ -24,3 +29,20 @@ def get_media_by_id(media_id):
 
 def search_by_title(search_str):
   return list(filter(lambda m: m['title'].lower().find(search_str.lower()) != -1, medias_db['medias'].values()))
+
+def add_media_list(media_list):
+  # check if received diect has all and valid keys 
+  for media in media_list:
+    if set(media.keys()) != JSON_FIELDS:
+      raise InvalidMediaObjectException
+  # Create database entries
+  for index, media in enumerate(media_list, start=1):
+    id = int(medias_db['count']) + index
+    media['id'] = id # Add id to media object
+    medias_db['medias'][str(id)] = media
+  medias_db['count'] += len(media_list)
+  with open(db_path, 'r+') as f:
+    json.dump(medias_db, f)
+
+def search_by_dirname(search_dirname):
+  return list(filter(lambda m: m['dirname'] == search_dirname, medias_db['medias'].values()))
--- src/exceptions.py
+++ src/exceptions.py
@@ -3,3 +3,6 @@ class NotFoundException(Exception):
 
 class WrongMediaTypeException(Exception):
     pass
+
+class InvalidMediaObjectException(Exception):
+    pass
